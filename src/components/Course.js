6e492d1837fe5b815e5ffeb3835491bb9a540001
Gavin Broekema <gavinbroekema@gmail.com>
2023-05-02 13:23:17 -0500

    Courses page display

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Course(props) {
   return (
-    <div className="flex">
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </div>
+  
+
+          <Link class="course--module course--link" to="/courses">
+            <h2 class="course--label">Course</h2>
+            <h3 class="course--title">{props.title}</h3>
+            <p>Course {props.id}</p>
+          </Link>
+
   );
 }
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -3,12 +3,10 @@ import { React, useState, useEffect } from "react";
 import Header from "./Header";
 import CourseList from "./CourseList";
 
-
 export default function Courses(props) {
   const [courses, setCourses] = useState("");
   const [loading, setLoading] = useState(true);
 
-  
   // Fetch courses data when the Courses component is rendered
   useEffect(() => {
     setLoading(true);
@@ -16,25 +14,16 @@ export default function Courses(props) {
     fetch("/api/courses")
       .then((res) => res.json())
       .then((data) => {
-        if(activeFetch) {
-          setCourses(data);
-          setLoading(false);
-        }
+        setCourses(data);
+        setLoading(false);
       });
-      return () => {activeFetch = false};
-  }, [loading]);
-
-
+  }, []);
 
   return (
     <>
       <Header />
-      {
-        (loading) 
-        ? <p>Loading...</p>
-        : <CourseList data={courses}/>
-      }
-     
+      {loading ? <p>Loading...</p> : <CourseList data={courses} />}
+      
     </>
-  )
+  );
 }
