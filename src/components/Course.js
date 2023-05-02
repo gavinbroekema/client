1ff34d059955ed35eb1d3891229ed31b219fb97c
Gavin Broekema <gavinbroekema@gmail.com>
2023-05-02 13:46:03 -0500

    Trouble getting course ID for URL

--- src/components/Course.js
+++ src/components/Course.js
@@ -3,9 +3,7 @@ import { Link } from "react-router-dom";
 
 export default function Course(props) {
   return (
-  
-
-          <Link class="course--module course--link" to="/courses">
+          <Link class="course--module course--link" to={{pathname: `courses/${props.id}`}}>
             <h2 class="course--label">Course</h2>
             <h3 class="course--title">{props.title}</h3>
           </Link>
